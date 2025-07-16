c5d8e91e434ba8c5b051c40942b76e6be94a284f
Melisa Gerceker <m.gerceker@student.tue.nl>
2025-07-16 21:42:24 +0300

    Completed java fundamentals exercises 6 and 7.

--- javascript-fundamentals/javascript-exercises/07_reverseString/reverseString.js
+++ javascript-fundamentals/javascript-exercises/07_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
+const reverseString = function (string) {
 
+    let tempStringArray = [];
+    let resultArray = [];
+
+    for (let i = 0; i < string.length; i++) {
+
+        tempStringArray.push(string.charAt(i));
+    }
+
+    console.log(tempStringArray);
+
+    for (let i = string.length-1; i >= 0; i--) {
+        resultArray.push(tempStringArray[i]);
+    }
+
+    console.log(resultArray);
+
+    return resultArray.join("");
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
