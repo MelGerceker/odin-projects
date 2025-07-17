0d734b56ee79fdd88dca6ba962d3dc2eed6a930a
Melisa Gerceker <m.gerceker@student.tue.nl>
2025-07-17 22:58:33 +0300

    Completed js basics exercise 8, working on exercise 9.

--- javascript-fundamentals/javascript-exercises/08_removeFromArray/removeFromArray.js
+++ javascript-fundamentals/javascript-exercises/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removeItems) {
+
+    // ... makes it a rest parameter
+
+    return arr.filter(item => !removeItems.includes(item));
 };
+
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- javascript-fundamentals/javascript-exercises/09_sumAll/sumAll.js
+++ javascript-fundamentals/javascript-exercises/09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (arrStart, arrEnd) {
+
+    let sum = 0;
+
+    let arr = [];
+
+    for (let i = 0; i < arrEnd - arrStart + 1; i++) {
+        arr.push(arrEnd - i);
+    }
+
+
+    const result = arr.reduce((total, currentItem) => {
+        return total + currentItem;
+    }, 0);
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
