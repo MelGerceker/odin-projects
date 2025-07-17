2c2864fec4e431b716d4ba0d51ce335a10514ede
Melisa Gerceker <m.gerceker@student.tue.nl>
2025-07-17 23:59:33 +0300

    Completed basic js exercises 9, 10 and 11.

--- javascript-fundamentals/javascript-exercises/09_sumAll/sumAll.js
+++ javascript-fundamentals/javascript-exercises/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
 const sumAll = function (arrStart, arrEnd) {
 
+    if (arrStart > arrEnd) {
+        let temp = arrStart;
+        arrStart = arrEnd;
+        arrEnd = temp;
+    }
+
+    if (arrEnd < 0 || arrStart < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(arrEnd) || !Number.isInteger(arrStart)) {
+        return "ERROR";
+    }
+
     let sum = 0;
 
     let arr = [];
@@ -8,15 +21,19 @@ const sumAll = function (arrStart, arrEnd) {
         arr.push(arrEnd - i);
     }
 
+    console.log(arr);
 
     const result = arr.reduce((total, currentItem) => {
         return total + currentItem;
     }, 0);
 
+    console.log(result);
+
+    return result;
 
 };
 
-
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
--- javascript-fundamentals/javascript-exercises/11_tempConversion/tempConversion.js
+++ javascript-fundamentals/javascript-exercises/11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // (x-32) * 5/9
+
+  let result = (temp - 32) * (5 / 9);
+
+  return Math.round(result * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function (temp) {
+  // (x * 9/5 +32)
+
+  let result = temp * (9 / 5) + 32;
+
+  return Math.round(result * 10) / 10;
+
 };
 
 // Do not edit below this line
