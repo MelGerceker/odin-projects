cbbf6d4e2d56cc1a983bdaf3cd8682158c392e35
Melisa Gerceker <m.gerceker@student.tue.nl>
2025-07-24 18:41:03 +0300

    Completed basics js exercise 14.

--- javascript-fundamentals/javascript-exercises/14_fibonacci/fibonacci.js
+++ javascript-fundamentals/javascript-exercises/14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = parseInt(n);
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    } else if (n > 2) {
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    } else {
+        return "OOPS"; // for negatives
+    }
 
 };
 
