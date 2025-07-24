2c7c564961b4de9858f362f5bf55a954b4e99ad0
Melisa Gerceker <m.gerceker@student.tue.nl>
2025-07-24 18:34:20 +0300

    Completed js basics exercise 13.

--- javascript-fundamentals/javascript-exercises/13_palindromes/palindromes.js
+++ javascript-fundamentals/javascript-exercises/13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    const cleaned = string
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, ""); // handles punctuation & spaces
+
+    const n = cleaned.length;
+
+    for (let i = 0; i <  Math.floor(n / 2); i++) {
+         if (cleaned[i] !== cleaned[n - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
