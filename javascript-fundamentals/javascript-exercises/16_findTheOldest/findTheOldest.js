1447a1a6d89704034c36da417e84d88e013c350c
Melisa Gerceker <m.gerceker@student.tue.nl>
2025-07-24 19:04:05 +0300

    Completed js basics exercise 15, exercise 16 is in process.

--- javascript-fundamentals/javascript-exercises/16_findTheOldest/findTheOldest.js
+++ javascript-fundamentals/javascript-exercises/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+
+    let age = array.map((people) => people.yearOfDeath - people.yearOfBirth);
+
+    const oldestAge =  Math.max(...age)
+
+    return oldestAge;
 
 };
+
+//console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
